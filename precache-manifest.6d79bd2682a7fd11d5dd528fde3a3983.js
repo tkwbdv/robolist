133871750bee86a44e40d1119f89611d9525950c
ThoKur <Mick.18@gmx.de>
2019-04-05 19:27:46 +0200

    Updates

---
+++ precache-manifest.6d79bd2682a7fd11d5dd528fde3a3983.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e65256cf6120a846d07e",
+    "url": "/robolist/static/css/main.828754e4.chunk.css"
+  },
+  {
+    "revision": "e65256cf6120a846d07e",
+    "url": "/robolist/static/js/main.19c45d85.chunk.js"
+  },
+  {
+    "revision": "01b3e57682c93592622d",
+    "url": "/robolist/static/js/runtime~main.65f2c703.js"
+  },
+  {
+    "revision": "b05a669064a87749d1b3",
+    "url": "/robolist/static/css/2.b5c161dc.chunk.css"
+  },
+  {
+    "revision": "b05a669064a87749d1b3",
+    "url": "/robolist/static/js/2.2e5272d6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robolist/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "acfb9078ef972a2c4c8b2a6280c6ce92",
+    "url": "/robolist/index.html"
+  }
+];
